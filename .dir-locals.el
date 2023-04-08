9ed5aee998f97d1dc3c63185956909013b16ca67
M.E.L <mel@rustbelt>
2023-04-08 17:44:40 +0200

    Oushine folding for Makefile

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((makefile-gmake-mode . ((eval . (progn
+				   (outshine-mode 1)
+				   (column-enforce-mode 1)))
+			 (fill-column . 70 )
+			 (column-enforce-column . 79))))
